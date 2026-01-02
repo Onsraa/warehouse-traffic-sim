afc6e42e7b87735203067b2e6db9fce543762ff2
Teddy <teddytruong@protonmail.com>
2026-01-02 13:04:07 +0100

    simplified file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use bevy::prelude::*;
+use warehouse_sim::plugins::warehouse::WarehousePlugins;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Warehouse Simulator".into(),
+                resolution: (1280, 720).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(WarehousePlugins)
         .run();
 }
